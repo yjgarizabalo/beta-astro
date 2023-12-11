2f35b2ed3a20c457ae276dbb1e6634fbdee1b17e
yairg <yjgarizabalo@gmail.com>
2023-12-11 08:19:47 -0500

    [beta-astro] add update

---
+++ src/services/spacex.ts
@@ -0,0 +1,30 @@
+import { type Doc, type APIBetaAstroSpaceX } from "../types/api.js";
+
+export const getLaunchesBy = async ({id}: {id: string}) => {
+  const res = await fetch(`https://api.spacexdata.com/v5/launches/${id}`);
+
+  const launch  = (await res.json()) as Doc;
+  return launch
+};
+
+
+export const getLaunches = async () => {
+  const res = await fetch("https://api.spacexdata.com/v5/launches/query", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      query: {},
+      options: {
+        sort: {
+          date_unix: "asc",
+        },
+        limit: 12,
+      },
+    }),
+  });
+
+  const { docs: launches } = (await res.json()) as APIBetaAstroSpaceX;
+  return launches
+};
